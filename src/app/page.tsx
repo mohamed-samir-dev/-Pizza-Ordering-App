b330cc4746e2d0c15bb851d08230d5441259da28
Pizza Developer <dev@pizzaapp.com>
2025-11-13 01:22:41 +0200

    feat: create homepage with header, hero, and pizza menu sections

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+
+import { HeroSection, PizzaMenu } from '../components';
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-black">
+      <HeroSection />
+      <PizzaMenu />
+    </div>
+  );
+}
