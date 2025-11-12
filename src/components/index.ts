c63a6d25422803598ed076b8891167815dac8e54
Pizza Developer <dev@pizzaapp.com>
2025-11-13 01:11:25 +0200

    feat: setup component organization with index exports

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+// Layout components
+export { default as Header } from './layout/header/Header';
+
+// UI components
+export { default as HeroSection } from './ui//hero/HeroSection';
+export { default as PizzaMenu } from './PizzaMenu';
