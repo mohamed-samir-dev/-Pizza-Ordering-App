4cbb05ade4983efd54574280075465f791b8c94d
Mohamed Samir Mouawad <114841411+mohamed-samir-dev@users.noreply.github.com>
2025-11-14 16:49:23 +0200

    feat: add actions index exports

---
+++ src/components/layout/header/actions/index.ts
@@ -0,0 +1,7 @@
+export { default as ActionButtons } from './main/ActionButtons';
+export { default as SearchButton } from './search/SearchButton';
+export { default as FavoritesButton } from './shopping/FavoritesButton';
+export { default as CartButton } from './shopping/CartButton';
+export { default as LoginButton } from './auth/LoginButton';
+export { default as UserProfile } from './auth/UserProfile';
+export { default as MobileMenuButton } from './navigation/MobileMenuButton';
