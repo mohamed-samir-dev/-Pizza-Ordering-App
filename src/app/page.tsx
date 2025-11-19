6af43a093b7329ba61f539a872a98e14efb67439
Mohamed Samir Mouawad <114841411+mohamed-samir-dev@users.noreply.github.com>
2025-11-19 22:28:27 +0200

    feat: update home page with OurChief component integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
 
-import { HeroSection, HeroIllustration, PizzaMenu } from '../components';
-
+import { HeroSection, HeroIllustration, PizzaMenu} from '../components';
+import OurChief from '../components/ourchiefs/ourchief';
 export default function Home() {
   return (
     <div className="min-h-screen bg-black">
       <HeroSection />
-      <PizzaMenu />
-      <HeroIllustration />
+      <div id="pizza-menu">
+        <PizzaMenu />
+      </div>
+      <div id="hero-illustration">
+        <HeroIllustration />
+      </div>
+      <OurChief />
     </div>
   );
 }
